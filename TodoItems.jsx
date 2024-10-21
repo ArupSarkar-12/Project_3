3d2b4c2be8ad68d5f830e9b3065f4ad9b5ffa220
ArupSarkar-12 <arup17969@gmail.com>
2024-10-22 00:15:01 +0530

    Add files via upload

---
+++ TodoItems.jsx
@@ -0,0 +1,19 @@
+import styles from "./TodoItems.module.css";
+import TodoItem from "./TodoItem";
+function TodoItems({ todoItems, onDeleteClick }) {
+  return (
+    <>
+      <div className={styles.itemContainer}>
+        {todoItems.map((item) => (
+          <TodoItem
+            key={item.name}
+            todoDate={item.dueDate}
+            todoName={item.name}
+            onDeleteClick={onDeleteClick}
+          ></TodoItem>
+        ))}
+      </div>
+    </>
+  );
+}
+export default TodoItems;
